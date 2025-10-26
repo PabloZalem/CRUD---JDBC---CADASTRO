ecfbf59e4302ae9d0ae329cfb05740c2e05417d5
Pablo Zalem <zalem.pablo@yahoo.com>
2025-10-26 15:42:41 -0300

    Criamos um metodo com retorno Cliente, onde utilizamos um caso contrario lanca a excecao

--- src/service/ClienteService.java
+++ src/service/ClienteService.java
@@ -1,14 +1,25 @@
+package service;
+
 import org.springframework.stereotype.Service;
+
+import repository.ClienteRepository;
+import entitys.Cliente;
 
 @Service
 public class ClienteService {
 	public final ClienteRepository repository;
 	
-	public ClienteService(UsuarioRepository repository) {
+	public ClienteService(ClienteRepository repository) {
 		this.repository = repository;
 	}
 	
 	public void salvarUsuario(Cliente cliente) {
-		repository.saveAndFlush(usuario);
+		repository.saveAndFlush(cliente);
+	}
+	
+	public Cliente buscarUsuarioPorNome(String nome) {
+		return repository.findByName(nome).orElseThrow(
+				() -> new RuntimeException("Nome nao encontrado")
+		);
 	}
 }
