b1f8433f5ac9346da245ca22a01cb9e031a8db0a
Pablo Zalem <zalem.pablo@yahoo.com>
2025-10-26 15:37:41 -0300

    Criamos o metodo findByNome, pois no JpaRepository nao possui

--- src/repository/ClienteRepository.java
+++ src/repository/ClienteRepository.java
@@ -1,5 +1,13 @@
+package repository;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import entitys.Cliente;
+
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+	Optional<Cliente> findByName(String nome);
 
 }
