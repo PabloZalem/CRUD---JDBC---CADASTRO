c3d825fbb37ea72a21a7f1a1a7b34402ba9172d6
Pablo Zalem <zalem.pablo@yahoo.com>
2025-10-27 18:40:26 -0300

    Metodo deletar cliente

--- src/repository/ClienteRepository.java
+++ src/repository/ClienteRepository.java
@@ -3,11 +3,15 @@ package repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.transaction.annotation.Transactional;
 
 import entitys.Cliente;
 
-public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>, QueryByExampleExecutor<Cliente>{
 
 	Optional<Cliente> findByName(String nome);
 
+	@Transactional
+	void deleteByName(String email);
 }
