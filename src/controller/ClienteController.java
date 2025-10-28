030301ac6f0fd83de85f64b6cf6a59902573a240
Pablo Zalem <zalem.pablo@yahoo.com>
2025-10-28 19:01:42 -0300

    Criacao do put mapping

--- src/controller/ClienteController.java
+++ src/controller/ClienteController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,6 +34,12 @@ public class ClienteController {
 	@DeleteMapping
 	public ResponseEntity<Void> deletarClientePorNome(@RequestParam String nome) {
 		clienteService.deletarClientePorNome(nome);
+		return ResponseEntity.ok().build();
+	}
+	
+	@PutMapping
+	public ResponseEntity<Void> atualizarClientePorId(@RequestParam Integer id, @RequestBody Cliente cliente) {
+		clienteService.atualizarClientePorId(id, cliente);
 		return ResponseEntity.ok().build();
 	}
 }
