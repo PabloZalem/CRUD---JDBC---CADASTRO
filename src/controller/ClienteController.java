1b26883b19e161e09f199e767acb78f411e55989
Pablo Zalem <zalem.pablo@yahoo.com>
2025-10-28 18:36:29 -0300

    criado delete mapping

--- src/controller/ClienteController.java
+++ src/controller/ClienteController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,5 +28,11 @@ public class ClienteController {
 	@GetMapping
 	public ResponseEntity<Cliente> buscarClientePorNome(@RequestParam String nome) {
 		return ResponseEntity.ok(clienteService.buscarClientePorNome(nome));
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<Void> deletarClientePorNome(@RequestParam String nome) {
+		clienteService.deletarClientePorNome(nome);
+		return ResponseEntity.ok().build();
 	}
 }
