7ca2c815d22afc850b33fcbbbbd5d1dfd0575657
Pablo Zalem <zalem.pablo@yahoo.com>
2025-12-01 20:06:58 -0300

    Criacao da classe para tratamento de excecao

---
+++ src/main/com/example/spring_imc/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,41 @@
+package main.com.example.spring_imc.exceptions;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<String> handleNullPointer(NullPointerException ex) {
+        return ResponseEntity.badRequest().body("Campos obrigatórios não podem ser nulos.");
+    }
+
+    @ExceptionHandler(ArithmeticException.class)
+    public ResponseEntity<String> handleArithmetic(ArithmeticException ex) {
+        return ResponseEntity.badRequest().body("Altura não pode ser zero.");
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrity(DataIntegrityViolationException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("Violação de integridade no banco.");
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntime(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<String> handleEmptyResult(EmptyResultDataAccessException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente não encontrado");
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest().body("ID inválido ou não informado");
+    }
+}
