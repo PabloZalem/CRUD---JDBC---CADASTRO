b9b2445471cbfd908ded16811385b4267da3097e
Pablo Zalem <zalem.pablo@yahoo.com>
2025-12-02 06:54:53 -0300

    Remocao de import

--- src/main/com/example/spring_imc/exceptions/GlobalExceptionHandler.java
+++ src/main/com/example/spring_imc/exceptions/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package main.com.example.spring_imc.exceptions;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
