7b005008c7c0b19fe25c1dbde62820d07631ddf7
Pablo Zalem <zalem.pablo@yahoo.com>
2025-12-06 16:07:47 -0300

    refatoracao

--- src/main/com/example/spring_imc/controllers/ProtegidoController.java
+++ src/main/com/example/spring_imc/controllers/ProtegidoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/protegido")
 public class ProtegidoController {
+    
     @GetMapping
     public String dadosProtegidos(Authentication authentication) {
         // O objeto Authentication é preenchido pelo seu filtro JWT
